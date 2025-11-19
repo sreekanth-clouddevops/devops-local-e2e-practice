42cc84b717ae0c4be1e41819d5b236d25bb3f263
Sreekanth Manthena <sreekanth.clouddevops@gmail.com>
2025-11-19 07:06:56 +0000

    Format Terraform files for Day5 pipeline

--- Week3/Day5-Terraform-CICD/envs/dev/backend-and-main.tf
+++ Week3/Day5-Terraform-CICD/envs/dev/backend-and-main.tf
@@ -24,9 +24,9 @@ provider "aws" {
 module "vpc" {
   source = "../../../Day3-Terraform-VPC/modules/vpc"
 
-  project_name     = "devops-local-lab"
-  environment      = var.environment
-  vpc_cidr         = "10.30.0.0/16"
+  project_name = "devops-local-lab"
+  environment  = var.environment
+  vpc_cidr     = "10.30.0.0/16"
 
   public_subnets  = ["10.30.1.0/24", "10.30.2.0/24"]
   private_subnets = ["10.30.11.0/24", "10.30.12.0/24"]
