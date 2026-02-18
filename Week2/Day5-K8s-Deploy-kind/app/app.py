1b0e1eaebe89e9eb3cbaf923e8e0cc9b7f5b0427
Sreekanth CloudDevOps <sreekanth.clouddevops@gmail.com>
2026-02-18 21:39:13 +0530

    Updated code

--- Week2/Day5-K8s-Deploy-kind/app/app.py
+++ Week2/Day5-K8s-Deploy-kind/app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins → Docker → Kind Cluster!"
+    return "Hello from Jenkins → Docker → Kind local Cluster!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
